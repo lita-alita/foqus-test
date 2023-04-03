0d98f239f5a049d97dfb4c1478c48e8218d53375
lita-alita <mrslitik@gmail.com>
2023-04-03 23:22:13 +0300

    add drag&drop behavior + bugs

--- js/categories.js
+++ js/categories.js
@@ -1,10 +1,60 @@
-const pointer = document.getElementById("categ-req-for-eachother-pointer");
-const subCategories = document.getElementById("categ-req-for-eachother-subcategories-list"); 
-
-function pointerHandler () {
-   pointer.classList.toggle("rotate") 
-   subCategories.classList.toggle("invisible")
-   console.log("point")
+const findIt = (data, event) => {
+   let search = event.target.value
+   clear(data)
+   document.getElementById("searchInput").value = search
+   
+   if (search) {
+       document.getElementById("close-image").classList.remove("invisible")
+       document.getElementById("search").classList.add("searchFocus")
+   } else {
+       document.getElementById("search").classList.remove("searchFocus")
+       document.getElementById("close-image").classList.add("invisible")
+   }
+   data.categ.remove(function(cat) {
+           let remove = cat.title.includes(search) || cat.description?.includes(search)
+           return !remove
+   })
 }
 
-pointer.addEventListener("click", pointerHandler)
+const clear = (data) => {
+   document.getElementById("searchInput").value = ""
+   data.categ.removeAll()
+   data.categories.map((cat) => data.categ.push(new CategoriesCreating(cat)))
+}
+
+const start = (target) => {
+   console.log("dragstart")
+   target.classList.add('selected')
+}
+
+const over = (target) => {
+   console.log("dragover")
+
+   target.classList.remove('selected')
+}
+
+const dragBehavior = (data, event) => {
+   console.log("data", data)
+
+   let listElement = document.getElementById("dropzone")
+   console.log(listElement.children)
+
+   start(event.target)
+
+  const activeElement = document.querySelector(`.selected`);
+
+  const currentElement = event.target;
+
+
+  if (currentElement.parentElement.classList.contains("_categorie")) {
+      currentElement.parentElement.classList.add("underline");
+  }
+  
+  const nextElement = (currentElement === activeElement.nextElementSibling) ?
+      activeElement.nextElementSibling :
+      currentElement;
+
+  listElement.insertBefore(activeElement, nextElement);
+  over(event.target)
+
+}
